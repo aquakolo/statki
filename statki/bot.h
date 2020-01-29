407b0707754b1d01abd9479e6fa4e862246bfcd1
aquakolo <aquakolo13@gmail.com>
2020-01-29 16:10:40 +0100

    repair

--- statki/bot.h
+++
@@ -1,5 +0,0 @@
-#include"else.h"
-void start_bot(int _level, int _board[][10]);
-int shoot(int x, int y);
-int turn();
-void answer(int x, int y, int ans);
--- statki/make.sh
+++
@@ -1,1 +0,0 @@
-gcc -Wno-format -o client client.c gtk.c else.c bot.c -Wno-deprecated-declarations -Wno-format-security -lm `pkg-config --cflags --libs gtk+-3.0` -export-dynamic
